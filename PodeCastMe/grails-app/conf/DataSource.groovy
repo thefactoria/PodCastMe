8992b63aa0307137fac22f353d0e396561e2aa96
Mohamed LARAKI <simolaraki@gmail.com>
2013-02-12 21:00:03 +0100

    closes #17 Configuration de la BD CloudBees
    Voir les infos de l'issue 1 pour l'installation de la Base en local

--- PodeCastMe/grails-app/conf/DataSource.groovy
+++ PodeCastMe/grails-app/conf/DataSource.groovy
@@ -1,21 +1,6 @@
 dataSource {
     pooled = true
-	dbCreate = "update"
-	url = "jdbc:mysql://localhost/db_podcastme"
-	driverClassName = "com.mysql.jdbc.Driver"
-	dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-	username = "root"
-	password = ""
-	properties {
-		maxActive = 50
-		maxIdle = 25
-		minIdle = 5
-		initialSize = 5
-		minEvictableIdleTimeMillis = 60000
-		timeBetweenEvictionRunsMillis = 60000
-		maxWait = 10000
-		validationQuery = "/* ping */"
-	}
+	
    // driverClassName = "org.h2.Driver"
 
    
@@ -32,13 +17,14 @@ environments {
 //            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
 //            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
       
-			pooled = true
-			dbCreate = "update"
-			url = "jdbc:mysql://localhost/db_podcastme"
-			driverClassName = "com.mysql.jdbc.Driver"
-			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
 			username = "root"
 			password = ""
+			dbCreate = "update"
+			driverClassName = "com.mysql.jdbc.Driver"
+			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+			url = "jdbc:mysql://localhost/db_podcastme"
+			pooled = true
+			
 			properties {
 				maxActive = 50
 				maxIdle = 25
@@ -53,20 +39,25 @@ environments {
     }
     test {
         dataSource {
-            dbCreate = "update"
-           // url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        
-			driverClassName = "com.mysql.jdbc.Driver"
-			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-			url = "jdbc:mysql://127.0.0.1/db_podcastme"
-			pooled = true
+//            dbCreate = "update"
+//           // url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//        
+//			driverClassName = "com.mysql.jdbc.Driver"
+//			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+//			url = "jdbc:mysql://127.0.0.1/db_podcastme"
+//			pooled = true
 			}
     }
     production {
         dataSource {
+			
+			username = "laraki"
+			password = "22222222"
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
+           // url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			driverClassName = "com.cloudbees.jdbc.Driver"
+			url = "[[jdbc:cloudbees://podcastme]]"
+			pooled = true
             properties {
                maxActive = -1
                minEvictableIdleTimeMillis=1800000
