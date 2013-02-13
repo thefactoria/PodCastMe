9f1d8d76245acc638f00aea2e98f8128077c459e
Morchedi <morchedi.marouen@gmail.com>
2013-02-13 03:39:22 +0200

    #3 redirection sur le contrlleur

---
+++ PodeCastMe/grails-app/controllers/podcastDomain/UserController.groovy
@@ -0,0 +1,128 @@
+package podcastDomain
+
+import org.springframework.dao.DataIntegrityViolationException
+
+class UserController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        [userInstanceList: User.list(params), userInstanceTotal: User.count()]
+    }
+
+    def create() {
+        [userInstance: new User(params)]
+    }
+
+    def save() {
+        def userInstance = new User(params)
+        if (!userInstance.save(flush: true)) {
+            render(view: "create", model: [userInstance: userInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])
+        redirect(action: "show", id: userInstance.id)
+    }
+
+    def show(Long id) {
+        def userInstance = User.get(id)
+        if (!userInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [userInstance: userInstance]
+    }
+
+    def edit(Long id) {
+        def userInstance = User.get(id)
+        if (!userInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [userInstance: userInstance]
+    }
+	def login = {
+		if (request.method == 'POST') {
+			def u = User.findByPseudoAndPasswd(params.pseudo, params.passwd)
+			if (u) {
+				// username and password match -> log in
+				session.user = u
+				if(u.isAdmin)
+				render(view:"/layouts/administration.gsp")
+				else
+				render(view:"/layouts/accueil.gsp")
+				//redirect(action: "list", params: params)
+			} else {
+			flash.message = "User not found"
+			render(view: "/inscription.gsp")
+			}
+		} else if (session.user) {
+		// don't allow login while user is logged in
+		render(view:"/index2.gsp")
+		}
+	}
+	
+	def logout = {
+		session.invalidate()
+		render(view:'/index')
+		}
+
+
+    def update(Long id, Long version) {
+        def userInstance = User.get(id)
+        if (!userInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (userInstance.version > version) {
+                userInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: 'user.label', default: 'User')] as Object[],
+                          "Another user has updated this User while you were editing")
+                render(view: "edit", model: [userInstance: userInstance])
+                return
+            }
+        }
+
+        userInstance.properties = params
+
+        if (!userInstance.save(flush: true)) {
+            render(view: "edit", model: [userInstance: userInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])
+        redirect(action: "show", id: userInstance.id)
+    }
+
+    def delete(Long id) {
+        def userInstance = User.get(id)
+        if (!userInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), id])
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            userInstance.delete(flush: true)
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'user.label', default: 'User'), id])
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'user.label', default: 'User'), id])
+            redirect(action: "show", id: id)
+        }
+    }
+}
